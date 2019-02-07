a3d72a80061d9038afd9258bd6cbfca20ee11859
Md Abedin <mdabedincs@gmail.com>
2019-02-06 23:13:36 -0500

    add functional and responsive twitter feed

--- src/js/modules/core/components/TwitterFeed.tsx
+++ src/js/modules/core/components/TwitterFeed.tsx
@@ -6,6 +6,8 @@ import { connect } from "react-redux";
 import { Theme } from "../../ThemeInjector";
 import { ReduxState } from "../../../reducers";
 import Underline from "./Underline";
+// @ts-ignore
+import { TwitterTimelineEmbed } from "react-twitter-embed";
 
 interface Props extends WithStyles<typeof styles> {
   user: User;
@@ -16,11 +18,20 @@ const styles = (theme: Theme) => ({
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    paddingTop: "50px",
+    paddingTop: "80px"
   },
   title: {
     fontSize: "2em",
     textTransform: "uppercase"
+  },
+  feed: {
+    paddingTop: "20px",
+    width: "600px"
+  },
+  [`@media(max-width: ${theme.mediumBreakpoint})`]: {
+    feed: {
+      width: "70vw"
+    }
   }
 });
 
@@ -29,6 +40,16 @@ const TwitterFeed: React.FunctionComponent<Props> = ({ user, classes }) => {
     <div className={classes.TwitterFeed}>
       <h1 className={classes.title}> Twitter Feed </h1>
       <Underline />
+      <div className={classes.feed}>
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="HackNYU"
+          noHeader={true}
+          noFooter={true}
+          noBorders={true}
+          options={{ height: 500 }}
+        />
+      </div>
     </div>
   );
 };
